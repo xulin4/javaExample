c7f08d885ad6704ae1400a95d87d1bba4617dc90
xulin <xulin@dcrays.cn>
2025-09-26 18:25:02 +0800

    Substring的两种构造方法

---
+++ src/main/java/org/example/String/SubString.java
@@ -0,0 +1,15 @@
+package org.example.String;
+
+public class SubString {
+    public static void main(String[] args) {
+        String a = "abc333333";
+        String b = a.substring(3);
+        System.out.println(b);
+        String c = a.substring(0,4);
+        System.out.println(c);
+        //SubString有两种构造方法，一种的参数是beginIndex,一种的参数beginIndex和endIndex
+
+
+
+    }
+}
